41a95c3bdd658751b05db946e826ed6500cd9603
max029113 nauduik <maks.naud@gmail.com>
2025-12-09 09:27:50 +0200

    added manual take_picture

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
+import json
 import requests
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for
 
-GO_server = "URL"
+GO_server = "http://localhost:8080/api/bot/activity"
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = 'D:\\KPI\\uploads'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
@@ -13,10 +14,13 @@ system_state = {
     "command_to_camera": 0
 }
 
-@app.route('/report_motion', methods=['POST'])
+@app.route('/report_motion', methods=['GET', 'POST'])
 def report_motion():
     system_state['command_to_camera'] = 1
-    return "Command received", 200
+    if request.method == 'POST':
+        return "Command received (via POST)", 200
+    else:
+        return "Command received (via Redirect)", 200
 
 @app.route('/check_command', methods=['GET'])
 def check_command():
@@ -29,23 +33,33 @@ def check_command():
 
 @app.route('/upload_photo', methods=['POST'])
 def upload_photo():
-    if 'imageFile' not in request.files:
-        return "No image file", 400
-        
     file = request.files['imageFile']
     filename = os.path.join(UPLOAD_FOLDER, f"capture_{os.urandom(4).hex()}.jpg")
     file.save(filename)
+    print(f"Photo saved: {filename}")
 
-    file_to_send = {
-        'image' : (filename, file.stream, file.mimetype)
-    }
     try:
-        ans = requests.post(GO_server, files=file_to_send)
-        print(f"GO server responded: {ans.status_code} {ans.text}")
+        with open(filename, 'rb') as f_out:
+            files_to_send = {'photo': f_out}
+            
+            ans = requests.post(GO_server, files=files_to_send)
+            
+            print(f"GO server responded: {ans.status_code} {ans.text}")
+            
     except Exception as e:
         print(f"Error sending to GO: {e}")
-    print(f"Photo saved: {filename}")
-    return "Saved", 200
+        return {"status": "error", "message": str(e)}, 500
+
+    return {"path": filename}, 200
+
+
+@app.route('/take_photo', methods=['GET','POST']) 
+def take_photo():
+    if request.method == 'POST':
+        pass
+    return redirect(url_for('report_motion'))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=True)
+
